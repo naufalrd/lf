84cbc72bc01ef1b5fde291c95866ea141417410b
yesyualeon <54468793+yesyualeon@users.noreply.github.com>
2021-03-28 16:07:50 +0700

    Replace dokter n perawat

--- application/views/perawat/home.php
+++ application/views/perawat/home.php
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 <div class="container mb-container">
     <div class="row mt-5 mx-5 p-5 shadow bg-white rounded">
         <h1 class="text-center">Tabel Cek Tensi</h1>
@@ -57,31 +56,4 @@
             </table>
         </div>
     </div>
-=======
-<div class="container mb-container">
-    <div class="row mt-5 mx-5 p-5 shadow bg-white rounded">
-        <h1 class="text-center">Tabel Cek Tensi</h1>
-        <div class="container text-center overflow-auto">
-            <a href="" class="btn btn-outline-dark mb-5">Panggil</a>
-            <table class="table">
-                <thead>
-                    <tr>
-                        <th scope="col">Nomor Antrian</th>
-                        <th scope="col">Nama Pasien</th>
-                        <th scope="col">Tensi</th>
-                        <th scope="col">Action</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <tr class="align-middle">
-                        <td>2</td>
-                        <td>Alicia</td>
-                        <td></td>
-                        <td><a href="" class="btn btn-success">tambah data</a></td>
-                    </tr>
-                </tbody>
-            </table>
-        </div>
-    </div>
->>>>>>> ef48e530a1fdae27d3745207c021d10046d4c322
 </div>
